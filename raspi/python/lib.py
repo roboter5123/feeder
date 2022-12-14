822ee6a34ac356d057304a02e38c6f7a5249fb01
roboter5123 <pascal.roboter@hotmail.com>
2022-12-14 20:40:47 +0100

    Added some docs and sped up json loading a bit

--- raspi/python/lib.py
+++ raspi/python/lib.py
@@ -8,23 +8,30 @@ motor_backward_pin = 18
 
 motor = Motor(motor_forward_pin, motor_backward_pin)
 
-def get_settings(_ = None):
+def get_settings() -> dict:
+    """
+    This method gets the settings from feeder/raspi/python/settings.json 
+    and returns them as a dict.
+    """
     
     if os.path.exists("feeder/raspi/python/settings.json"):
         
         json_settings = open("feeder/raspi/python/settings.json", "r")
-        settings = json_settings.read()
+        settings = json.load(json_settings)
         json_settings.close()
     
         return settings
     
     else:
         
-        return ""
+        return {}
 
-def set_settings(settings):
-    
-    settings = json.loads(settings)
+def set_settings(settings: dict) -> None:
+    """
+    This method sets the settings in feeder/raspi/python/settings.json.
+    If the file doesn't exist it creates it.
+    If the file exists it parses the settings, replaces unmatching ones and adds in non existent ones.
+    """
     
     if os.path.exists("feeder/raspi/python/settings.json"):
         
@@ -45,11 +52,11 @@ def set_settings(settings):
         settings_file.close()
         return
 
-    json_settings = json.loads(settings_file_string)
+    json_settings = settings
     
-    for setting in settings:
+    for setting in json.loads(settings_file_string):
         
-        if not json_settings.get(setting, False):
+        if not json_settings.get(setting, False) or json_settings.get(setting, False) != settings.get(setting):
             
             json_settings[setting] = settings[setting]
             
@@ -59,7 +66,7 @@ def set_settings(settings):
         
     return
 
-def dispense(seconds):
+def dispense(seconds: int) -> None:
 
     motor.forward()
     print(f"turning for {seconds} seconds")
