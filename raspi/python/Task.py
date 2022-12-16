b4a93bfde49eda7915e1fb85efe84cd673e5ad07
roboter5123 <pascal.roboter@hotmail.com>
2022-12-16 21:33:55 +0100

    Added type declaration in Task

--- raspi/python/Task.py
+++ raspi/python/Task.py
@@ -3,7 +3,7 @@ class Task:
     time: str
     dispense_seconds:int
     
-    def __init__(self, t, ds):
+    def __init__(self, t:str, ds:int):
         
         self.time = t
         self.dispense_seconds = ds
