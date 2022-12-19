7164d9f58d2de2919a966925a0838026d2676400
roboter5123 <pascal.roboter@hotmail.com>
2022-12-19 13:50:39 +0100

    Added docs

--- raspi/python/socket_server.py
+++ raspi/python/socket_server.py
@@ -10,7 +10,10 @@ server_functions = {}
 port = 8058
 
 def start_sockets():
-    
+    """
+    Main method of the socket connection to the main sever.
+    Establishes connection and listens for commands. Then executes them.
+    """
     global server_functions
     
     while True:
@@ -32,19 +35,24 @@ def start_sockets():
             continue
         
 def start_outgoing_socket():
+    """
+    Creates the socket and connects to the main server.
+    """
     
-        global server
+    global server
     
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("connecting")
-        server.connect((login_url, port))
-        print("connected")
-        print("Successfully connected")
-        lib.log("Successfully connected to login server")
-        return
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("connecting")
+    server.connect((login_url, port))
+    print("connected")
+    print("Successfully connected")
+    lib.log("Successfully connected to login server")
+    return
                 
 def listen_for_instructions():
-    
+    """
+    Listens for instructions and executes them with their arguments.
+    """
     global server
     
     while True:
@@ -71,7 +79,10 @@ def listen_for_instructions():
         server.sendall(response.encode())
 
 def set_settings(args: list) -> bool:
-    
+    """
+    Sets settings from arguments received over socket connection.
+    Arguments should be a a list with only 1 settings dictionary in it.
+    """
     global settings
     global sched
     
@@ -86,10 +97,19 @@ def set_settings(args: list) -> bool:
     return str(True)
 
 def get_settings(args: list) -> dict:
-    
+    """
+    Gets the current settigs and returns them to be sent back to the server.
+    """
     return json.dumps(lib.dictify_settings())
 
 def add_task_from_connection(args: list) -> bool:
+    """
+    Adds a task to the schedule from socket connection.
+    Args should be a list with: 
+    int representing a weekday on index 0
+    str representing time of day on index 1
+    int representing seconds of dispensing on index 2
+    """
     
     day = Weekday(int(args[0]))
     time = args[1]
@@ -98,7 +118,11 @@ def add_task_from_connection(args: list) -> bool:
     return str(True)
 
 def dispense_from_connection(args: list) -> bool:
-    
+    """
+    Dispensese from socket connection
+    args should be a list with:
+    int representing seconds of dispensing on index 0
+    """
     lib.dispense(int(args[0]))
     return str(True)
         
